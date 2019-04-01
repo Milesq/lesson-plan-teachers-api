9fce9efe2a56f772663add7ab7fcb468cad0dd91
Milesq <wmilosz88@gmail.com>
2019-04-01 15:30:28 +0200

    Add filter teachers

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 const getPlan = require('./getPlanFromUrl');
 
 module.exports = async code => {
-    const plan = await getPlan('http://zsm1.bydgoszcz.pl/1plan/plany/o8.html');
+    let plan = await getPlan('http://zsm1.bydgoszcz.pl/1plan/plany/o8.html');
+    plan = plan.map(hour => {
+        return hour.filter(lesson => lesson.teacher === code);
+    });
 
     return JSON.stringify(plan, null, 2);
 };
