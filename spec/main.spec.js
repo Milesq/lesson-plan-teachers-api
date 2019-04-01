33501fb519f4823b31383a08abce4396848b74e2
Milesq <wmilosz88@gmail.com>
2019-04-01 16:03:32 +0200

    Add tests

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,5 +1,19 @@
-describe('d', () => {
-    it('s', () => {
-        expect(2).toBe(2);
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
+const { clear } = require('../app/getPlanFromUrl.js');
+const { JSDOM } = require('JSDOM');
+
+describe('Clear function', () => {
+    it('exist', () => {
+        expect(typeof clear).toBe(typeof(() => {}));
+        expect(clear.length).toBe(1);
+    });
+
+    it('work', () => {
+        const before = readFileSync(resolve('./spec/helpers/htmlBeforeClean.txt')),
+            after = readFileSync(resolve('./spec/helpers/htmlAfterClean.txt'));
+        let content = (new JSDOM(before)).window.document;
+        content = content.querySelectorAll('table')[2];
+        expect(clear(content)).toBe(after);
     });
 });
